d7bfea8e659f736516f3408665a7c919cc49f26e
earacena <emanuel.aracena57@myhunter.cuny.edu>
2019-08-17 22:59:38 -0400

    Update Chip8.cpp

--- Chip8.cpp
+++ Chip8.cpp
@@ -14,28 +14,28 @@ Chip8::Chip8(const std::string & game_name)
   display_.set_width( 640 );
   display_.initialize();
 
-	cpu_.load_game(game_name);	
-        cpu_.initialize();
+  cpu_.load_game(game_name);	
+  cpu_.initialize();
 }
 
 void Chip8::run()
 {
   //uint64_t cycle_number = 0;
-        while ( display_.is_running() ) {
-                while ( display_.poll_event( event_ ) ) {
-                        if ( event_.type == sf::Event::Closed )
-                                display_.close_window();
-                }
+  while ( display_.is_running() ) {
+    while ( display_.poll_event( event_ ) ) {
+      if ( event_.type == sf::Event::Closed )
+        display_.close_window();
+    }
 
-		//std::cout << std::dec << "Cycle " << cycle_number << ":" << std::endl << "\t";
-                cpu_.emulate_cycle();
-                //++cycle_number;
+    //std::cout << std::dec << "Cycle " << cycle_number << ":" << std::endl << "\t";
+    cpu_.emulate_cycle();
+    //++cycle_number;
 	       
-                display_.clear_screen();
+    display_.clear_screen();
 		
-                if ( cpu_.get_draw_status() == 1)
+    if ( cpu_.get_draw_status() == 1)
 		  display_.draw_graphics(cpu_.get_screen());
 
-                display_.update_screen();
-        }
+    display_.update_screen();
+  }
 }
