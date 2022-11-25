fd6825ce1c5d6db469937549d54eaca75ebc8c09
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-11-24 22:26:45 -0500

    Add handling for closing emulator or debugger

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -6,25 +6,27 @@
 
 #include "Chip8.h"
 
-
 //      Constructor
-Chip8::Chip8(const std::string & game_name)
-{
-  display_.set_height( 320 );
-  display_.set_width( 640 );
+Chip8::Chip8(const std::string& game_name) {
+  display_.set_height(320);
+  display_.set_width(640);
   display_.initialize();
 
   cpu_.load_game(game_name);
 }
 
-void Chip8::run(Debugger * debugger = nullptr)
-{
+void Chip8::run(Debugger* debugger = nullptr) {
   while (display_.is_running()) {
-    while (display_.poll_event( event_ )) {
-      if ( event_.type == SDL_QUIT ) {
+    while (display_.poll_event(event_)) {
+      if (debugger != nullptr) ImGui_ImplSDL2_ProcessEvent(&event_);
+
+      if (event_.type == SDL_QUIT ||
+          (event_.type == SDL_WINDOWEVENT &&
+           event_.window.event == SDL_WINDOWEVENT_CLOSE &&
+           (event_.window.windowID == display_.get_window_id() ||
+            event_.window.windowID == display_.get_window_id()))) {
         cleanup();
-        if (debugger != nullptr) 
-          debugger->cleanup();
+        if (debugger != nullptr) debugger->cleanup();
       } else if (event_.type == SDL_KEYUP) {
         cpu_.update_key_states(event_);
       }
@@ -34,20 +36,15 @@ void Chip8::run(Debugger * debugger = nullptr)
 
     // Draw changes on screen
     display_.clear_screen();
-    if ( cpu_.draw_flag == 1)
-      display_.draw_graphics(cpu_.screen);
+    if (cpu_.draw_flag == 1) display_.draw_graphics(cpu_.screen);
     display_.update_screen();
 
     // Render and update debugger window
-    if (debugger != nullptr)
-      debugger->render(&cpu_);
+    if (debugger != nullptr) debugger->render(&cpu_);
 
     // Sleep to slow down emulation
     // std::this_thread::sleep_for(std::chrono::milliseconds(2));
   }
-  
 }
 
-void Chip8::cleanup() {
-  display_.close();
-}
+void Chip8::cleanup() { display_.close(); }
